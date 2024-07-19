390fa502eaa9c31985b88a157fe248fc01dd2780
Artur Dulius <duliusartur@gmail.com>
2024-07-19 18:37:01 -0300

    Entrega parte dos planos e porque da ferramenta - Site

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ window.addEventListener('scroll', function() {
         body.classList.remove('scrolled');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const defaultImage = 'images/logo-plans-default.png';
+    const selectedImage = 'images/logo-plans-selected.png';
+
+    const updateImages = function() {
+        document.querySelectorAll('.plan-logo').forEach(img => img.src = defaultImage);
+        const checkedRadio = document.querySelector('.input-plans:checked');
+        if (checkedRadio) {
+            checkedRadio.nextElementSibling.querySelector('.plan-logo').src = selectedImage;
+        }
+    };
+
+    document.querySelectorAll('.input-plans').forEach(radio => {
+        radio.addEventListener('change', updateImages);
+    });
+
+    updateImages();
+});
